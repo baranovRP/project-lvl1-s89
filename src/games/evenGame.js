6eea6874bd4c222a0d31f28d639529e0be2f3344
Roman Baranov <baranov.r.p@gmail.com>
2017-03-15 18:57:53 +0100

    [lvl1-s89-05] Refactor imports

--- src/games/evenGame.js
+++ src/games/evenGame.js
@@ -1,11 +1,4 @@
-import {
-  greetMsg,
-  congratsMsg,
-  successMsg,
-  failureMsg,
-  helloMsg,
-  questionMsg,
-} from '../lib/message';
+import * as msg from '../lib/message';
 import * as util from '../lib/util';
 
 const yes = 'yes';
@@ -17,23 +10,23 @@ const isAnswerCorrect = (answer, num) =>
   (isEven(num) ? (answer === yes) : (answer === no));
 
 const evenGame = (attemptTotal) => {
-  util.print(greetMsg('Answer "yes" if number even otherwise answer "no".\n'));
+  util.print(msg.greetMsg('Answer "yes" if number even otherwise answer "no".\n'));
   const userName = util.getName();
-  util.print(helloMsg(userName));
+  util.print(msg.helloMsg(userName));
 
   const iter = (attemptCount, num) => {
     if (attemptTotal <= attemptCount) {
-      util.print(congratsMsg(userName));
+      util.print(msg.congratsMsg(userName));
       return;
     }
 
-    util.print(questionMsg(num));
+    util.print(msg.questionMsg(num));
     const actualAnswer = util.getAnswer();
     const isCorrect = isAnswerCorrect(actualAnswer.toLowerCase(), num);
     const expectedAnswer = isEven(num) ? yes : no;
 
     const message = isCorrect ?
-      successMsg() : failureMsg(actualAnswer, expectedAnswer, userName);
+      msg.successMsg() : msg.failureMsg(actualAnswer, expectedAnswer, userName);
 
     util.print(message);
     iter(attemptCount + 1, util.getRandomInt());
