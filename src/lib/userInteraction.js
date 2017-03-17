6e5874feacd80daeda7f09d334155ef4e99d65cd
Roman Baranov <baranov.r.p@gmail.com>
2017-03-17 18:38:55 +0100

    [lvl1-s89-07] remove redundant file

--- src/lib/userInteraction.js
+++
@@ -1,7 +0,0 @@
-import readlineSync from 'readline-sync';
-
-export const getName = () => readlineSync.question('May I have your name? ');
-
-const getAnswer = () => readlineSync.question('Your answer: ');
-
-export default getAnswer;
